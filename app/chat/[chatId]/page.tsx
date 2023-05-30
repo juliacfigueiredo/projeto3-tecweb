8479b02287e1fc6baf53ce024b921fe0c5ab9ebb
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2023-05-30 10:36:22 -0300

    Começo do page Page chatid

--- app/chat/[chatId]/page.tsx
+++ app/chat/[chatId]/page.tsx
@@ -1,10 +1,11 @@
-import { Input } from '@chakra-ui/react'
-
+'use client';
+import { Heading } from '@chakra-ui/react'
 export default function page({ params }: { params: { chatId: string } }) {
   return (
     <div>
         {params.chatId}
-        
+        <Heading>Chat</Heading>
+
     </div>
   )
 }
